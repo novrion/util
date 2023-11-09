d214d7bd01d2b66a93058d1bc984bba1e1ddcc5a
novrion <128396601+novrion@users.noreply.github.com>
2023-11-09 09:50:46 +0100

    Update Euler_step_method.cpp

--- math/Euler_step_method.cpp
+++ math/Euler_step_method.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
+#include <cmath>
 
 
 
 inline double Derivative(const double x, const double y) {
-  return 2 * x;
+    return 2 * sqrt(y);
 }
+
 
 
 int main() {
 
-  double xs, xe, ys, ye;
-  double step;
+    double xs, xe, ys, ye;
+    double step;
 
-  std::cout << "x start: "; 
-  std::cin >> xs;
-  std::cout << "x end: ";
-  std::cin >> xe;
-  std::cout << "y start: ";
-  std::cin >> ys;
-  std::cout << "step: ";
-  std::cin >> step;
+    std::cout << "x start: "; 
+    std::cin >> xs;
+    std::cout << "x end: ";
+    std::cin >> xe;
+    std::cout << "y start: ";
+    std::cin >> ys;
+    std::cout << "step: ";
+    std::cin >> step;
 
-
-  for (xs; xs < xe; xs += step) {
-    ye += Derivative(cur_x, 0) * step;
-  }
-
-
-  std::cout << ye;
-  std::cin >> step;
+    ye = ys;
+    for (xs; xs <= xe; xs += step) {
+        ye += Derivative(xs, ye) * step;
+    }
 
 
+    std::cout << ye;
+    std::cin >> step;
 
 
 
-  return 0;
+
+
+    return 0;
 }
